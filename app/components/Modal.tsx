2775501199477b29fea44fe11b4b2a974cb6e630
woongsnote <woongsnote@gmail.com>
2023-08-22 12:55:30 +0900

    feat: add link to modal
    - add unsplash link to modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Dispatch, SetStateAction } from "react";
 
 export default function Modal({
@@ -19,15 +20,20 @@ export default function Modal({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-      <div className="w-[600px]">
+      <div className="w-2/5">
         <div className="bg-white p-2 flex flex-col">
           <input
             type="text"
-            className="pl-2"
+            className="border-2 border-blue-500 w-full p-1 placeholder:italic placeholder:text-slate-400 block bg-white rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring-blue-500 focus:ring-1 sm:text-sm"
             placeholder="Image Url을 입력하세요!"
             value={imageUrl}
             onChange={(e) => setImageUrl(e.target.value)}
           />
+
+          <Link href="https://unsplash.com/wallpapers" className="mt-4 text-sm">Unsplash 바로가기</Link>
+
+
+
           <div className="flex flex-row justify-center gap-12 items-center mt-2">
             <button
               className={`${buttonStyle} text-blue-500`}
